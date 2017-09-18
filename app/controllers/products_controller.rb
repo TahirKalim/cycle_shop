eadd187a791c91a6cc1c85f5507c6a91fb6da39b
tahirkalim <thrkalim@gmail.com>
2017-09-18 23:43:29 +0200

    4.2 added color field to products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,7 +15,7 @@ class ProductsController < ApplicationController
   # GET /products/new
   def new
     @product = Product.new
-  end
+  end 
 
   # GET /products/1/edit
   def edit
@@ -69,6 +69,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :description, :image_url)
+      params.require(:product).permit(:name, :description, :image_url, :colour)
     end
 end
